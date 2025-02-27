f50344ebf84a39387d2a340f40bec3e9de4bb035
boris_medoshin <jetun@ya.ru>
2025-02-27 17:19:13 +0300

    multiple window example

--- electron/main.ts
+++ electron/main.ts
@@ -1,32 +1,52 @@
-import { app, BrowserWindow } from 'electron';
-import * as path from 'node:path';
+import path from 'path'
+import { app, BrowserWindow } from 'electron'
 
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+const windows: BrowserWindow[] = []
 
-async function run() {
-    //const distPort = CT_ENV.port;
-    let win: BrowserWindow | null = null;
+function createWindow() {
+  // Renderer 1
+  const win = new BrowserWindow()
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(path.posix.join(process.env.VITE_DEV_SERVER_URL, 'html/index.html'))
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/html/index.html'))
+  }
+  windows.push(win)
 
-    await app.whenReady();
-    app.on('window-all-closed', () => {
-        app.quit();
-        win = null;
-    });
+  // Renderer 2
+  const nodeTrue = new BrowserWindow({
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+    width: 700,
+    height: 500,
+  })
+  if (process.env.VITE_DEV_SERVER_URL) {
+    nodeTrue.loadURL('http://localhost:5174/html/node-true.html')
+  } else {
+    nodeTrue.loadFile(path.join(__dirname, '../dist/html/node-true.html'))
+  }
+  windows.push(nodeTrue)
 
-    win = new BrowserWindow({
-        webPreferences: {
-            devTools: true,
-            webSecurity: false,
-            nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js'), // add "preload"
-        },
-    });
-    //win.webContents.openDevTools();
-
-    // run server
-    //const serverInfo = runServer(path.join(__dirname, '..', 'dist', 'app-nms'));
-    //console.log('----- opened dist server on: ', serverInfo.address());
-    await win.loadURL(`https://google.com`);
+  // Renderer 3
+  const nodeFalse = new BrowserWindow({
+    width: 600,
+    height: 400,
+  })
+  if (process.env.VITE_DEV_SERVER_URL) {
+    nodeFalse.loadURL('http://localhost:5175/html/node-false.html')
+  } else {
+    nodeFalse.loadFile(path.join(__dirname, '../dist/html/node-false.html'))
+  }
+  windows.push(nodeFalse)
 }
 
-run();
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  windows.length = 0
+  app.quit()
+})
--- vite.config.mts
+++ vite.config.mts
@@ -1,28 +1,31 @@
 import electron from 'vite-plugin-electron/simple';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-
+import path from 'node:path';
+import multiple from 'vite-plugin-multiple';
 
 export default {
-    build: {
-        minify: false,
-    },
     plugins: [
         electron({
             main: {
-
                 entry: 'electron/main.ts',
             },
             preload: {
-                input: 'electron/preload.ts',
+                input: path.join(__dirname, 'electron/preload.ts'),
             },
         }),
-        viteStaticCopy({
-            targets: [
-                //{
-                //src: `../${CT_ENV.distDir}/*`,
-                //dest: 'app-nms',
-                //}
-            ],
-        }),
+        multiple([
+            {
+                name: 'node-true',
+                config: 'vite.node-true.config.mts',
+            },
+            {
+                name: 'node-false',
+                config: 'vite.node-false.config.mts',
+            },
+        ]),
     ],
+    build: {
+        rollupOptions: {
+            input: path.join(__dirname, 'html/index.html'),
+        },
+    },
 };
